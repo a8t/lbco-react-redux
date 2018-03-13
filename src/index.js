b7607552b9550c043260db06aa5a20f46fc16565
Andy Tran <andy8tran@gmail.com>
2018-03-12 21:22:30 -0400

    Add react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), root);
+
 registerServiceWorker();

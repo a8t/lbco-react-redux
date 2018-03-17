b03e40c888443619663ecf65da95729b4b38354a
Andy Tran <andy8tran@gmail.com>
2018-03-17 09:32:01 -0400

    Remove extra stylesheet

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import LocationSearchContainer from 'containers/LocationSearchContainer/LocationSearchContainer'
 
 class App extends Component {

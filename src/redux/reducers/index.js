cf089746c4f4d331e8cdda7bf706f01be1245ce8
Andy Tran <andy8tran@gmail.com>
2018-03-17 09:33:28 -0400

    Add reducer index

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import locationSearch from 'redux/reducers/locationSearch';
+
+const rootReducer =  combineReducers({locationSearch});
+
+export default rootReducer;
